bdeeebde67e72d63d16973c20e9284708a8aea03
shriyaTiwari <shriyatravel2018@gmail.com>
2021-03-06 15:55:38 +0530

    added the path

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ var engine, world;
 var box1, pig1,pig3;
 var backgroundImg,platform;
 var bird, slingshot;
-
+var gameState;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -41,6 +41,8 @@ function setup(){
 
     //log6 = new Log(230,180,80, PI/2);
     slingshot = new SlingShot(bird.body,{x:200, y:50});
+
+    gameState = "on sling"
 }
 
 function draw(){
@@ -69,16 +71,21 @@ function draw(){
 }
 
 function mouseDragged(){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if(gameState==="on sling"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    }
+  
 }
 
 
 function mouseReleased(){
     slingshot.fly();
+    gameState = "released"
 }
 
 function keyPressed(){
     if(keyCode === 32){
         slingshot.attach(bird.body);
+        gameState = "on sling"
     }
 }
